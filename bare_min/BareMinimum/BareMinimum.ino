efe7bd63bb784351b755bea2448ee6d3c43c3506
Vincent Howard <vincenguyenhoward@gmail.com>
2024-12-30 20:43:31 -0700

    basics of led lights

---
+++ bare_min/BareMinimum/BareMinimum.ino
@@ -0,0 +1,51 @@
+void setup() {
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(13, HIGH);
+    delay(250);   
+    digitalWrite(12, HIGH);
+    delay(250); 
+    digitalWrite(11, HIGH);
+    delay(250); 
+    digitalWrite(10, HIGH);
+    delay(250); 
+
+    digitalWrite(13, LOW);
+    delay(250);   
+    digitalWrite(12, LOW);
+    delay(250); 
+    digitalWrite(11, LOW);
+    delay(250); 
+    digitalWrite(10, LOW);
+    delay(250); 
+
+    delay(500); 
+    digitalWrite(13, HIGH); 
+    digitalWrite(12, HIGH);
+    digitalWrite(11, HIGH);
+    digitalWrite(10, HIGH);
+
+    delay(500); 
+    digitalWrite(13, LOW); 
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+    digitalWrite(10, LOW);
+
+    delay(500); 
+    digitalWrite(13, HIGH); 
+    digitalWrite(12, HIGH);
+    digitalWrite(11, HIGH);
+    digitalWrite(10, HIGH);
+
+    delay(500); 
+    digitalWrite(13, LOW); 
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+    digitalWrite(10, LOW);
+    delay(1000); 
+}
